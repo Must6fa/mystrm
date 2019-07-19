bc95589bdef69810638f15a77415637f354e1848
Must6fa <52785285+Must6fa@users.noreply.github.com>
2019-07-20 01:09:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ var prefix = "-";
 var adminprefix = '-'
 
 
-const developers = ["598251914458562574"]
+const developers = ["599198480036003850"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
